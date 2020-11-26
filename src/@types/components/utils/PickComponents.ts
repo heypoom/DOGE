6398f5e78c43f0dc3d52506dbfd5ce1c2d71534e
Phoomparin Mano <phoomparin@gmail.com>
2020-11-26 16:39:05 +0700

    feat: improve type of PickComponents

--- src/@types/components/utils/PickComponents.ts
+++ src/@types/components/utils/PickComponents.ts
@@ -2,14 +2,9 @@ import type { ConditionalExcept } from 'type-fest'
 
 import type { IComponentType, IComponentMap } from '..'
 
-export type IComponentOfKey<
-  T extends IComponentType[],
-  K extends IComponentType
-> = T extends (infer U)[] ? (K extends U ? IComponentMap[K] : never) : never
-
 export type PickComponents<
-  T extends IComponentType[],
+  T extends IComponentType,
   R = {
-    [K in IComponentType]: IComponentOfKey<T, K>
+    [K in IComponentType]: K extends T ? IComponentMap[K] : never
   }
 > = ConditionalExcept<R, never>
--- src/@types/entities/utils/schema.ts
+++ src/@types/entities/utils/schema.ts
@@ -1,7 +1,7 @@
 import type { IComponentType, PickComponents } from '../../components'
 
 export function Schema<T extends IComponentType[]>(...v: T) {
-  type Entity = PickComponents<T>
+  type Entity = PickComponents<T[number]>
 
   return {} as Entity
 }

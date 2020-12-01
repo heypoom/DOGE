1b2bb607294c91d9b522b9121c84e90c57ef2db2
Phoomparin Mano <phoomparin@gmail.com>
2020-12-01 22:43:01 +0700

    feat: use template literal types to generate handlers

--- src/@types/core/ISystem.ts
+++ src/@types/core/ISystem.ts
@@ -1,20 +1,11 @@
-import type { ISystemHandler } from './ISystemHandler'
+import type { ISystemLifecycleHandlers } from './ISystemLifecycle'
 
 import type { IComponentType } from '../../@types/components'
 
-export interface ISystem<T extends IComponentType[] = IComponentType[]> {
+export type ISystem<T extends IComponentType[] = IComponentType[]> = {
   /** Name of the system */
   name?: string
 
   /** Which components does this system need? */
   query?: T
-
-  /** Run this system handler on setup */
-  onSetup?: ISystemHandler<T>
-
-  /** Run this handler on tick (update) */
-  onTick?: ISystemHandler<T>
-
-  /** Run this system handler on cleanup (removed) */
-  onCleanup?: ISystemHandler<T>
-}
+} & ISystemLifecycleHandlers<T>
--- src/@types/core/ISystemLifecycle.ts
+++ src/@types/core/ISystemLifecycle.ts
@@ -1,1 +1,9 @@
+import type { ISystemHandler } from './ISystemHandler'
+
+import type { IComponentType } from '../components'
+
 export type ISystemLifecycle = 'setup' | 'tick' | 'cleanup'
+
+export type ISystemLifecycleHandlers<T extends IComponentType[]> = {
+  [x in ISystemLifecycle as `on${Capitalize<x>}`]?: ISystemHandler<T>
+}
